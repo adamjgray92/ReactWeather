43c482b7bc54b0a6a3acff4185f0d5005e563989
Adam Gray <adamgray@Adams-MBP.default>
2017-09-12 13:00:11 +0100

    Changed About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,15 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
+      <h1 className="text-center">About</h1>
+      <p className="text-center">This is a weather application built on React. I learned this from the React Udemy Course!</p>
+      <p className="text-center">
+        Here are some of the tools I've Used:
+      </p>
+      <ul>
+        <li><a href="https://facebook.github.io/react">React</a> - This is the Javascript framework used.</li>
+        <li><a href="https://openweathermap.org">Open Weather Map</a> - I use Open Weather Map to search for weather data by city name.</li>
+      </ul>
     </div>
   );
 };
